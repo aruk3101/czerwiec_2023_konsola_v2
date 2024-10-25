e50e8fdd1b93f3b33019cc1666bbdf53d913e83b
Damian Raczek <draczekprojekt@gmail.com>
2024-10-25 09:41:55 +0200

    Filling A table with true boolean values implemented.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,12 +1,23 @@
-﻿namespace ConsoleApp1
+﻿using System.Runtime.InteropServices;
+
+namespace ConsoleApp1
 {
     internal class Program
     {
         static void Main()
         {
-            
+            Console.WriteLine("Liczby pierwsze od 2 do 100: " + string.Join(", ", SitoEratostenesa(100)));
         }
 
-       
+        static List<int> SitoEratostenesa(int n)
+        {
+            bool[] A = new bool[n + 1];
+            for (int i = 2; i <= n; i++)
+            {
+                A[i] = true;
+            }
+
+            return null;
+        }
     }
 }
