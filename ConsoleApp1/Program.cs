04837b2f988d9ae71c66cfd0e47c2c4e27df048a
Damian Raczek <draczekprojekt@gmail.com>
2024-10-25 09:45:46 +0200

    Sieve of Eratosthenes algorithm finished and implemented as well as displaying the result with proper message.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -9,6 +9,20 @@ namespace ConsoleApp1
             Console.WriteLine("Liczby pierwsze od 2 do 100: " + string.Join(", ", SitoEratostenesa(100)));
         }
 
+        static void WypelnijTablice(bool[] tablica)
+        {
+            for (int i = 2; i <= Math.Sqrt(tablica.Length - 1); i++)
+            {
+                if (tablica[i])
+                {
+                    for (int j = i * 2; j <= tablica.Length - 1; j += i)
+                    {
+                        tablica[j] = false;
+                    }
+                }
+            }
+        }
+
         static List<int> SitoEratostenesa(int n)
         {
             bool[] A = new bool[n + 1];
@@ -17,7 +31,18 @@ namespace ConsoleApp1
                 A[i] = true;
             }
 
-            return null;
+            WypelnijTablice(A);
+
+            List<int> liczbyPierwsze = new List<int>();
+            for (int i = 2; i <= n; i++)
+            {
+                if (A[i])
+                {
+                    liczbyPierwsze.Add(i);
+                }
+            }
+
+            return liczbyPierwsze;
         }
     }
 }
